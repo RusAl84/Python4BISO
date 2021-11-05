a4398e6faa6ce5bf512e2124b6658840ddb16158
Rusakov Aleksey <34722284+RusAl84@users.noreply.github.com>
2021-11-05 14:07:39 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+def main():
+    # mas = [[1,2],[3,4]]
+    # print(mas*3)
+    # mas =[]
+    # for i in range(8):
+    #     item=[i,2,3,4,5]
+    #     mas.append(item)
+    # print(mas)
+    a = np.array([1,2,3])
+    print(a)
+    print(a*a)
+    print(type(a))
+
+if __name__ == '__main__':
+    main()
